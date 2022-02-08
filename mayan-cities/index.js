99fe3172edf3357b283e6bc696ae2112051ec462
swiftzerr <swiftzermc@gmail.com>
2022-02-08 13:43:24 -0500

    Updated more website stuffs

--- mayan-cities/index.js
+++ mayan-cities/index.js
@@ -1,11 +1,12 @@
 window.onscroll = function() {scrollFunction()};
+window.onload = function () {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.padding = "10px 10px";
+        document.getElementById("navbar").style.padding = "3px 10px";
         document.getElementById("title").style.fontSize = "25px";
     } else {
-        document.getElementById("navbar").style.padding = "40px 10px";
+        document.getElementById("navbar").style.padding = "20px 10px";
         document.getElementById("title").style.fontSize = "35px";
     }
 }
